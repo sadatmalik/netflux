c47e78efc7561172a2d7c08dc727c0006aaaa896
sadat.malik <sadat.malik@me.com>
2022-01-04 19:43:35 +0000

    implementing movie API rest controller

---
+++ src/main/java/com/sadatmalik/netflux/controllers/MovieController.java
@@ -0,0 +1,29 @@
+package com.sadatmalik.netflux.controllers;
+
+import com.sadatmalik.netflux.domain.Movie;
+import com.sadatmalik.netflux.services.MovieService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/movies")
+@RequiredArgsConstructor
+public class MovieController {
+
+    private final MovieService movieService;
+
+    @GetMapping("/{id}")
+    Mono<Movie> getMovieById(@PathVariable String id) {
+        return movieService.getMovieById(id);
+    }
+
+    @GetMapping
+    Flux<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+}
